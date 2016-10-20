62b8e3d6d854d3fbe09d84b19b7839c6407e7a52
rpedro10 <rpedro10@gmail.com>
2016-10-20 21:57:22 +0100

    teste
    teste

---
+++ src/uc.h
@@ -0,0 +1,17 @@
+/*
+ * Ucs.h
+ */
+
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Uc {
+	int semestre;
+	string nome;
+	int ano;
+public:
+	Uc(int semestre,int ano,int sem);
