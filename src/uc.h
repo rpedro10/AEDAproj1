ddc2d93d52a74a33739c3e04ec52b5d061b48265
rpedro10 <rpedro10@gmail.com>
2016-10-24 17:23:59 +0100

    uc.h done
    done

--- src/uc.h
+++ src/uc.h
@@ -10,8 +10,34 @@
 using namespace std;
 
 class Uc {
-	int semestre;
 	string nome;
+	string sigla;
+	int semestre;
 	int ano;
+	float creditos;
+//	vector<Aluno> inscritos;
+
 public:
 	Uc(int semestre,int ano,int sem);
+};
+
+class Optativa: public Uc {
+	int vagas;
+	string curso;
+	string faculdade;
+	string area;
+public:
+	Optativa(string nome,string sigla,int semestre,int ano,float cred,int vagas,string curso,string fac,string area);
+
+};
+
+class N_Optativa: public Uc {
+
+public:
+	N_Optativa(string nome,string sigla,int semestre,int ano,float cred);
+
+};
+
+
+
+
